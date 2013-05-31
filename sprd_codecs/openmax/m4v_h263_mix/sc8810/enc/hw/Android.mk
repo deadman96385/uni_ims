2550c7a2e75e087bc6e75ab080a3977321c5d5d7
xiaowei.luo <xiaowei.luo@spreadtrum.com>
2013-05-31 23:28:09 +0800

    Bug#172229, add 8810(7710) mp4 encoder core library.
    Change-Id: I395388d5968a3f5facf153bdcd78f6ddf13b575a

--- sprd_codecs/openmax/m4v_h263_mix/sc8810/enc/hw/Android.mk
+++ sprd_codecs/openmax/m4v_h263_mix/sc8810/enc/hw/Android.mk
@@ -20,26 +20,20 @@ LOCAL_SRC_FILES := \
 	src/mp4enc_trace.c \
 	src/mp4enc_vlc.c \
 	src/mp4enc_vop.c \
-	../../../vsp/sc8810/src/vsp_drv_sc8810.c
+	../../../../vsp/sc8810/src/vsp_drv_sc8810.c
 
 
-LOCAL_MODULE := libsprdm4vencoder
+LOCAL_MODULE := libomx_m4vh263enc_hw_sprd
 LOCAL_MODULE_TAGS := optional
 
-
-LOCAL_CFLAGS := -fno-strict-aliasing -D_VSP_LINUX_  -D_VSP_  -DCHIP_ENDIAN_LITTLE  -DCHIP_8810  $(PV_CFLAGS)
+LOCAL_CFLAGS := -fno-strict-aliasing -D_VSP_LINUX_  -D_VSP_  -DCHIP_ENDIAN_LITTLE  -DCHIP_8810
 LOCAL_ARM_MODE := arm
 
-LOCAL_STATIC_LIBRARIES := 
-
-LOCAL_SHARED_LIBRARIES := 
+LOCAL_SHARED_LIBRARIES := \
+	libutils
 
 LOCAL_C_INCLUDES := \
-	$(PV_TOP)/codecs_v2/video/m4v_h263_sprd/sc8810/enc/include \
-	$(PV_TOP)/codecs_v2/video/vsp/sc8810/src \
-	$(PV_TOP)/codecs_v2/video/vsp/sc8810/inc \
- 	$(PV_INCLUDES)
+	$(LOCAL_PATH)/include \
+	$(TOP)/vendor/sprd/proprietories-source/sprd_codecs/openmax/vsp/sc8810/inc \
 
-LOCAL_COPY_HEADERS_TO := $(PV_COPY_HEADERS_TO)
-
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
