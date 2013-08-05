5a0ec4a16c8ccb2a88a48ff85f6de94dd4f5c291
xiaowei.luo <xiaowei.luo@spreadtrum.com>
2013-08-05 21:01:08 +0800

    Bug#193662, add IO command to close mm clock when close vsp
    Change-Id: I6f6f94bea6fce33aa6f97df6e7e21d69be402693

--- sprd_codecs/openmax/vsp/sc8830/src/vsp_drv_sc8830.c
+++ sprd_codecs/openmax/vsp/sc8830/src/vsp_drv_sc8830.c
@@ -56,6 +56,7 @@ PUBLIC void VSP_CLOSE_Dev(VSPObject *vo)
 
     if(vo->s_vsp_fd > 0)
     {
+		ioctl(vo->s_vsp_fd, VSP_RELEASE_MMCLK, &ret);
         munmap(vo->s_vsp_fd,SPRD_VSP_MAP_SIZE);
         close(vo->s_vsp_fd);
     }
