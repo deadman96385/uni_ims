2a2c741bd22ab4f7075b7179495c992d1fea476d
Patrick Shi <patrick.shi@spreadtrum.com>
2015-11-26 17:35:59 +0800

    Revert "Bug #502420 Porting whale secure boot to 51 trunk"
    [root cause  ] Porting whale secure boot to 51 trunk
    [changes     ] Porting whale secure boot to 51 trunk
    [side effects] none
    [self test   ] <board, Test cases summary>:whale cell download tools & boot ok
    [reviewers   ] Adam.Sun
    [change_type ] feature
    [tag_product ] common
    
    This reverts commit c1ac18f4bf59ae18a10ce3f53a9dfae90567d78a.
    
    Change-Id: Ifb6d1fea19b0164a00d9b2e1b74187ffbbae8e7a

--- packimage/signimage/sansa/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := sansa_sign.sh
-LOCAL_SRC_FILES := sansa_sign.sh
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_IS_HOST_MODULE := true
-include $(BUILD_PREBUILT)
-include $(LOCAL_PATH)/source/Android.mk
