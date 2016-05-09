f78cc9eb0d0ae2b5fcf16f70582822cd26be7af3
zhangyy@spreadst.com <zhangyy@spreadst.com>
2016-05-09 17:39:14 +0800

    Bug #555524 Number replacement, leading to the call interface display two-way telephone
    [root cause  ] Number replacement, leading to the call interface display two-way telephone
    [changes     ] Number of the same does not replace, resulting in a call interface display two-way telephone
    [side effects] no
    [self test   ]<board, Test cases summary>:<9830><follow bug steps,test 10 times,pass><TE3,pass>
    [reviewers   ] Dialer
    
    [change_type ] feature_bugfix#541566
    [tag_product ] common
    
    Change-Id: I7b478459160747f245619cbbcc9500b41fb67567

--- ims/src/com/spreadtrum/ims/ImsCallSessionImpl.java
+++ ims/src/com/spreadtrum/ims/ImsCallSessionImpl.java
@@ -99,6 +99,8 @@ public class ImsCallSessionImpl extends IImsCallSession.Stub {
             CommandsInterface ci, ImsServiceCallTracker callTracker){
         mImsDriverCall = new ImsDriverCall(dc);
         updateImsCallProfileFromDC(dc);
+        // SPRD:add for bug555524
+        updateFromDc(dc);
         mContext = context;
         mCi = ci;
         mImsServiceCallTracker = callTracker;
@@ -135,6 +137,8 @@ public class ImsCallSessionImpl extends IImsCallSession.Stub {
         updateImsCallProfileFromDC(dc);//SPRD:add for bug523375 updata dc when fallback to voice
         switch(dc.state){
             case DIALING:
+             // SPRD:add for bug555524
+                mState = ImsCallSession.State.INITIATED;
                 try{
                     if (mImsDriverCall == null && mIImsCallSessionListener != null) {
                         mIImsCallSessionListener.callSessionProgressing((IImsCallSession) this,
