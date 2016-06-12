c2e8e8fa23b543500e3e7070c7b2000e1d04ccd6
yinghan <ying.han@spreadtrum.com>
2016-06-12 09:36:22 +0800

    Bug #567448 avoid to get frame out of sequence
    [root cause  ] If JB only have SPS,PPS and I frame and there is no smaller TS packets, there will be the case after
    the first taking I frame to take SPS PPS frame, resulting in subsequent decoding problems caused mosaic.
    [changes     ] files in /video
    [side effects] no
    [self test   ]<board, Test cases summary>:[sp9860g_1h10] video call OK
    [reviewers   ] wenmei.hua,chengli.du
    [change_type ] PLD_Feature_Management
    [tag_product ] common
    
    Change-Id: I9ca5efdc3ac74bab98066d816050f0da3956309f
    (cherry picked from commit 1be3e95335a18d7d0b9bd663463bb9c8162f41b3)
    (cherry picked from commit 4495d94996ce77d86595ce53bc741043082fe37a)

--- video/video_2_0/jbv/_jbv.c
+++ video/video_2_0/jbv/_jbv.c
@@ -328,6 +328,7 @@ vint _JBV_findTsMax(
     uint64    foundTs;
     uint16    seqn;
     uint16    xseqn;
+    vint      mSeqn;
     vint      m1Seqn;
     vint      nextSeqn;
     JBV_Unit *unit_ptr;
@@ -338,6 +339,7 @@ vint _JBV_findTsMax(
      */
     tsMax = 0;
     foundTs = tsMax;
+    mSeqn = -1;
     m1Seqn = -1;
     for (seqn = 0; seqn < _JBV_SEQN_MAXDIFF; seqn++) {
         unit_ptr = &obj_ptr->unit[seqn];
@@ -391,6 +393,7 @@ vint _JBV_findTsMax(
                 break;
             }
             if (obj_ptr->unit[xseqn].firstInSeq) {
+                mSeqn = seqn;
                 m1Seqn = xseqn;
                 foundTs = tsMax;
                 break;
@@ -403,14 +406,14 @@ vint _JBV_findTsMax(
         }
     }
 
-    if (m1Seqn < 0) {
+    if (mSeqn < 0) {
         /* No sequence complete in JB. */
         JBV_dbgLog("No sequence complete in JB\n");
         return (JB_NO_PACKETS_READY);
     }
 
-    *ts_ptr = obj_ptr->unit[m1Seqn].ts;
-    return (m1Seqn);
+    *ts_ptr = obj_ptr->unit[mSeqn].ts;
+    return (mSeqn);
 }
 
 
