291bb698d4f818bd6e56cb9c73774449b0663dd7
liangliang.cao <liangliang.cao@spreadtrum.com>
2016-08-29 17:27:37 +0800

    Bug#585726  the merge button do not show in callbutton list  by chance [root cause  ]revert this commit [changes     ]revert this commit [side effects]no [self test   ]no [reviewers   ]michael.shi [change_type ] [tag_product ] common [self test   ]ok
    Change-Id: I6fa037ca2078ca4d9cb17fd37d0ce3014e456e97

--- ril/sprd_ril/sprd_ril.c
+++ ril/sprd_ril/sprd_ril.c
@@ -12336,7 +12336,7 @@ static void initializeCallback(void *param)
     int channelID;
     channelID = getChannel();
     setRadioState (channelID, RADIO_STATE_OFF);
-     RILLOGD("sprdroid6.0_trunk_16b_16.25.5__p4_lib_patch_cus_20160819_V1.0_bug590779");
+     RILLOGD("sprdroid6.0_trunk_16b_16.25.5__p4_lib_patch_cus_20160829_V1.0_bug585726");
 
 #ifdef SIM_AUTO_POWERON
 #if 0
@@ -12797,6 +12797,16 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
             || strStartsWith(s,"NO CARRIER")
             || strStartsWith(s,"+CCWA")
             ) {
+          if(s_ims_registered){
+                RIL_onUnsolicitedResponse (
+                     RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED,
+                     NULL, 0);
+           } else {
+                RIL_onUnsolicitedResponse (
+                     RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED,
+                     NULL, 0);
+           }
+
     } else if (strStartsWith(s,"+CREG:")
             || strStartsWith(s,"+CGREG:")
             ) {
@@ -13704,7 +13714,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
                     RIL_requestTimedCallback(redialWhileCallFailed,
                             (char *) strdup(response->number), NULL);
                 } else {
-                    if(response->type == 1 || response->type == 3/*ps call*/){
+                    if(s_ims_registered){
                         RIL_onUnsolicitedResponse (
                             RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED,
                             NULL, 0);
@@ -13740,8 +13750,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
                 }
 #endif
             } else {
-                    if(response->type == 1 || response->type == 3/*ps call*/){
-                         RILLOGD("get location fail");
+                    if(s_ims_registered){
                         RIL_onUnsolicitedResponse (
                             RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED,
                             NULL, 0);
@@ -13820,7 +13829,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
             }
 #endif
             if (response->type == 0) {
-                    if(response->type == 1 || response->type == 3/*ps call*/){
+                    if(s_ims_registered){
                         RIL_onUnsolicitedResponse (
                             RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED,
                             NULL, 0);
@@ -13832,7 +13841,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
                 goto out;
             } else if (response->type == 1) {
 #if defined (RIL_SPRD_EXTENSION)
-                if(response->type == 1 || response->type == 3/*ps call*/){
+                if(s_ims_registered){
                  RIL_onUnsolicitedResponse (
                         RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED,
                         NULL, 0);
@@ -13957,7 +13966,7 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
             RILLOGD("get exit_cause fail");
             goto out;
         }
-        if (response->cs_mod == 0) {
+        if (s_ims_registered) {
             RIL_onUnsolicitedResponse(RIL_UNSOL_RESPONSE_IMS_CALL_STATE_CHANGED, response,sizeof(RIL_IMSPHONE_CMCCSI));
         }
     } else if (strStartsWith(s, "+CMCCSS")) {
@@ -14097,6 +14106,9 @@ static void onUnsolicited (const char *s, const char *sms_pdu)
 #if defined (RIL_SPRD_EXTENSION)
     else if (strStartsWith(s,AT_PREFIX"DVTRING:")
             || strStartsWith(s,AT_PREFIX"DVTCLOSED")) {
+            RIL_onUnsolicitedResponse (
+               RIL_UNSOL_RESPONSE_VIDEOCALL_STATE_CHANGED,
+               NULL, 0);
     } else if (strStartsWith(s, AT_PREFIX"VTMDSTRT")) {
         int response;
         int index = 0;
