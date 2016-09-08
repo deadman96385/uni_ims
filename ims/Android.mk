907e4c4b05f81918e45d02c7a0a985cf9425f348
yingying <yingying.fan@spreadtrum.com>
2016-09-08 18:50:51 +0800

    Bug #537862 [Vowifi] To support IMS vowifi feature.
    [root cause  ] To support IMS vowifi feature.
    [changes     ] To support IMS vowifi feature.
    [side effects] none
    [self test   ]<board, Test cases summary>:<board: 9832a, test case: 1. boot test; 2. VoWifi sannity test>
    [reviewers   ] sally.he
    [change_type ] feature
    [tag_product ] specific
    
    Change-Id: If08f9c297b88557198f7c7c7a5afef89a41e9656
    (cherry picked from commit 3431398495e3c150a96fe25a9097877450321650)

--- ims/Android.mk
+++ ims/Android.mk
@@ -2,18 +2,26 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+vowifi_adapter_dir := ../../sprd_vowifi/source_code/VowifiAdapter
+
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := ims
 LOCAL_CERTIFICATE := platform
-LOCAL_JAVA_LIBRARIES := telephony-common ims-common
-
+LOCAL_JAVA_LIBRARIES := telephony-common ims-common radio_interactor_common
+LOCAL_STATIC_JAVA_LIBRARIES := vowifi_aidl
 LOCAL_DEX_PREOPT := nostripping
-
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PROTOC_OPTIMIZE_TYPE := micro
-res_dirs := res
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES += $(call all-java-files-under, $(vowifi_adapter_dir)/src)
+
+res_dirs := res $(vowifi_adapter_dir)/res
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 include $(BUILD_PACKAGE)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
