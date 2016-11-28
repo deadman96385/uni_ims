41764caf42188db759512e0f3c89d2d1ce11f61f
liangliang.cao <liangliang.cao@spreadtrum.com>
2016-11-28 11:07:17 +0800

    bug#621374 update audio quality
    [bug number ]bug#621374
    [root cause ]audio quality is base cs_mode
    [changes ]  modify audio quality base on mediaDescription
    [side effects ]nothing
    [reviewers ] michael shi
    [self test ]<board, Test cases summary>:<board: 9832><test case: following bug test steps. test 5 times, pass>
    [change_type ] AOB --> google_original
    [tag_product ] common
    
    Change-Id: I7505122a7376979ba2c0907159f64b35173a4aa1
    (cherry picked from commit 2a10521e2bd2da31a023c32d1b1d40a23c702844)

--- ims/src/com/spreadtrum/ims/ImsCallSessionImpl.java
+++ ims/src/com/spreadtrum/ims/ImsCallSessionImpl.java
@@ -971,7 +971,14 @@ public class ImsCallSessionImpl extends IImsCallSession.Stub {
             String qualityString = null;
             int quality = -1;
             if(index >=0){
-                qualityString = vdc.mediaDescription.substring(index+8);
+                int proIndex = vdc.mediaDescription.indexOf("\\hd=");
+                if(proIndex >= (index+8)){
+                   Log.i(TAG,"updateVideoProfile...proIndex & index="+ proIndex + "&" + index);
+                   qualityString = vdc.mediaDescription.substring(index+8,proIndex);
+                }else{
+                   Log.i(TAG,"updateVideoProfile...proIndex & index="+ proIndex + "&" + index);
+                   qualityString = vdc.mediaDescription.substring(index+8);
+                }
                 if(qualityString != null){
                     try {
                         quality = Integer.parseInt(qualityString);
