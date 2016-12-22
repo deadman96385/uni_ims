183d77dcf8a525ce225f693ae22d7140432e029d
bin.liang <bin.liang@spreadtrum.com>
2016-12-22 14:02:19 +0800

    Bug #630680 add interface for set NetworkSpecialRATCap
    [root cause  ] While Enabling the CFU, there is Network Error Popup
    [changes     ] add interface for setting NetworkSpecialRATCap to decide use UT or CS, according to the current operators AP determine the channel to send AT+SPOPRCAP set.
    [side effects] no
    [self test   ]<board, Test cases summary>:<9832, Enabling the CFU, test whether Network Error Popup, pull log ,check whether send AT+SPOPRCAP>
    [reviewers   ] zhiwei.fu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I73404de2114b8677a83c442ee88abb68d9ae97ee

--- ril/sprd_libril/sprd_ril_commands.h
+++ ril/sprd_libril/sprd_ril_commands.h
@@ -256,6 +256,7 @@
     ,{RIL_EXT_REQUEST_QUERY_LTE_CTCC_SIMTYPE, dispatchVoid, responseInts}
     ,{RIL_EXT_REQUEST_QUERY_LTE_CTCC_MMEI, dispatchVoid, responseString}
     ,{RIL_EXT_REQUEST_QUERY_DCE_SOFTWARE_VERSION, dispatchVoid, responseString}
+    ,{RIL_EXT_REQUEST_SET_SPECIAL_RATCAP, dispatchInts, responseVoid}
 #endif
 #endif
 #if defined (GLOBALCONFIG_RIL_SAMSUNG_LIBRIL_INTF_EXTENSION)
--- ril/sprd_ril/sprd_ril.c
+++ ril/sprd_ril/sprd_ril.c
@@ -9319,6 +9319,7 @@ onRequest (int request, void *data, size_t datalen, RIL_Token t)
                 || request == RIL_EXT_REQUEST_QUERY_LTE_CTCC_SIMTYPE
                 || request == RIL_EXT_REQUEST_QUERY_LTE_CTCC_MMEI
                 || request == RIL_EXT_REQUEST_QUERY_DCE_SOFTWARE_VERSION
+                || request == RIL_EXT_REQUEST_SET_SPECIAL_RATCAP
 #endif
 #endif
                 || request == RIL_REQUEST_REPORT_STK_SERVICE_IS_RUNNING
@@ -11906,6 +11907,21 @@ onRequest (int request, void *data, size_t datalen, RIL_Token t)
         case RIL_EXT_REQUEST_QUERY_DCE_SOFTWARE_VERSION:
             requestDceSoftwareVersion(channelID, data, datalen, t);
             break;
+        case RIL_EXT_REQUEST_SET_SPECIAL_RATCAP:{
+            p_response = NULL;
+            int value = ((int*)data)[0];
+            char cmd[32] = {0};
+            snprintf(cmd, sizeof(cmd), "AT+SPOPRCAP=1,1,%d", value);
+            err = at_send_command(ATch_type[channelID], cmd, &p_response);
+            if (err < 0 || p_response->success == 0) {
+                RIL_onRequestComplete(t, RIL_E_GENERIC_FAILURE, NULL, 0);
+            } else {
+                RIL_onRequestComplete(t, RIL_E_SUCCESS, NULL, 0);
+            }
+
+            at_response_free(p_response);
+            break;
+        }
 #endif  // RIL_SUPPORTED_OEMSOCKET
 #elif defined (GLOBALCONFIG_RIL_SAMSUNG_LIBRIL_INTF_EXTENSION)
         case RIL_REQUEST_GET_CELL_BROADCAST_CONFIG:
