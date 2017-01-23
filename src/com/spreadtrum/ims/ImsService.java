7f46010f3a37905efc315d21c177fbd565ed236e
kun.chen <kun.chen@spreadtrum.com>
2017-01-23 11:03:15 +0800

    Bug #627365 9832 fail to show Call waiting pop up , even when waiting is enabled
    [root cause ] not update mInCallPhoneId when create session
    [changes ] update mInCallPhoneId when create session
    [side effects] no
    [self test ]<board, Test cases summary>:<board:9832><test case:following bug test steps, test 6 times, pass>
    [reviewers ] lei.huang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Iaa87350b529dbfd521a3d6f33c1c45793377d3b5

--- src/com/spreadtrum/ims/ImsService.java
+++ src/com/spreadtrum/ims/ImsService.java
@@ -371,6 +371,7 @@ public class ImsService extends Service {
                         ImsServiceImpl service = mImsServiceImplMap.get(
                                 Integer.valueOf(ImsRegister.getPrimaryCard(mPhoneCount)+1));
                         service.sendIncomingCallIntent((String)msg.obj);
+                        mInCallPhoneId = ImsRegister.getPrimaryCard(mPhoneCount);//sprd:add for bug635699
                         updateInCallState(true);
                         Log.i(TAG,"EVENT_WIFI_INCOMING_CALL-> callId:"+msg.obj);
                         break;
@@ -947,6 +948,7 @@ public class ImsService extends Service {
             Log.e (TAG,"createCallSession->mIsVowifiCall: " + mIsVowifiCall
                     + " mIsVolteCall: " + mIsVolteCall + " isVoWifiEnabled(): " + isVoWifiEnabled()
                     + " isVoLTEEnabled(): " + isVoLTEEnabled());
+            mInCallPhoneId = serviceId-1;//SPRD:add for bug635699
             updateInCallState(true);
             if(!isImsEnabled()){
                 Log.e (TAG,"createCallSession-> ims is disable!");
@@ -977,6 +979,7 @@ public class ImsService extends Service {
         public IImsCallSession getPendingCallSession(int serviceId, String callId){
             Log.i (TAG," getPendingCallSession-> serviceId: " + serviceId + "  callId: " + callId + " mIsVowifiCall: " + mIsVowifiCall
                     + " mIsVolteCall: " + mIsVolteCall + " isVoWifiEnabled(): " + isVoWifiEnabled() + " isVoLTEEnabled(): " + isVoLTEEnabled());
+            mInCallPhoneId = serviceId-1;//SPRD:add for bug635699
             updateInCallState(true);
             /*SPRD: Modify for bug586758{@*/
             if(!isImsEnabled()){
