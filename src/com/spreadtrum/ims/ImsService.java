c30a9386602b72957060c436f3906be9a6f88c80
longchen.chen <longchen.chen@spreadtrum.com>
2017-05-27 14:06:53 +0800

    Bug #681991 There is a fail message in status bar when vowifi register fail
    [root cause  ] As customer request, remove vowifi fail message
    [changes     ] As customer request, remove vowifi fail message
    [side effects] no
    [self test   ]<9832A, when  vowifi register fail, there should not be a fail message on status bar>:
    [reviewers   ] longchen.chen
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I45e885af2a2e7903a168b296046d71ff0951d683

--- src/com/spreadtrum/ims/ImsService.java
+++ src/com/spreadtrum/ims/ImsService.java
@@ -501,8 +501,9 @@ public class ImsService extends Service {
                                         ? ImsOperationType.IMS_OPERATION_SWITCH_TO_VOWIFI
                                                 : ImsOperationType.IMS_OPERATION_HANDOVER_TO_VOWIFI);
                                 requestService.notifyImsHandoverStatus(ImsHandoverResult.IMS_HANDOVER_REGISTER_FAIL);
-                                Toast.makeText(ImsService.this, R.string.vowifi_register_error,
-                                        Toast.LENGTH_LONG).show();
+                                // SPCSS00387344 CUSTOMER need remove toast fail message.
+//                                Toast.makeText(ImsService.this, R.string.vowifi_register_error,
+//                                        Toast.LENGTH_LONG).show();
                                 /*SPRD: Modify for bug604833{@*/
                                 if(mFeatureSwitchRequest.mEventCode== ACTION_SWITCH_IMS_FEATURE) {
                                     if (requestService != null){
@@ -1390,7 +1391,9 @@ public class ImsService extends Service {
        @Override
        public void showVowifiNotification(){
            Log.i(TAG,"showVowifiNotification");
-           mNotificationManager =(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+           // SPCSS00387344 CUSTOMER need remove toast fail message.
+           return;
+           /*mNotificationManager =(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
            mNotificationManager.cancel(mCurrentVowifiNotification);
            Intent intent = new Intent();
            intent.setClassName("com.android.settings","com.android.settings.Settings$WifiCallingSettingsActivity");
@@ -1402,7 +1405,7 @@ public class ImsService extends Service {
            Notification notification = new Notification(android.R.drawable.stat_sys_warning,mVowifiRegisterMsg, System.currentTimeMillis());
            notification.flags = Notification.FLAG_AUTO_CANCEL;
            notification.setLatestEventInfo(ImsService.this, vowifiTitle, vowifiContent, contentIntent);
-           mNotificationManager.notify(mCurrentVowifiNotification, notification);
+           mNotificationManager.notify(mCurrentVowifiNotification, notification);*/
        }
        /* @} */
 
