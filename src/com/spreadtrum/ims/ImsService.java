5b2b7186732baa8affabfe07fffcfc32aa4c69f7
kun.chen <kun.chen@spreadtrum.com>
2017-06-19 16:33:32 +0800

    Bug #627365 don not registe viwifi when call end
    [root cause  ] do not call vowifi register
    [changes     ] delay update call state
    [side effects] no
    [self test   ]<board, Test cases summary>:<board: 9820><test case: following bug test steps, test 5 times, pass> [reviewers ] lei.huang
    [reviewers   ] lei.huang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I223de72e3401ad4e8e5e686fe4401652a40a0e0d
    (cherry picked from commit 913e881c298a0590fb9051dde7f3fd213ac5f0ad)

--- src/com/spreadtrum/ims/ImsService.java
+++ src/com/spreadtrum/ims/ImsService.java
@@ -1892,10 +1892,7 @@ class MyVoWifiCallback implements VoWifiCallback {
                 if (impl != null) {
                     if (impl.isVolteSessionListEmpty() && impl.isVowifiSessionListEmpty()) {
                         mCallEndType = CallEndEvent.VOLTE_CALL_END;
-                        if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
-                            Log.i(TAG,"onSessionEmpty->mCurrentImsFeature:"+mCurrentImsFeature);
-                            updateInCallState(false);
-                        }
+
                         /*SPRD: Modify for bug595321 and 610799{@*/
                         Log.i(TAG,"onSessionEmpty-> mFeatureSwitchRequest:"+mFeatureSwitchRequest + " mIsVowifiCall:" + mIsVowifiCall + " mIsVolteCall:" + mIsVolteCall +" mInCallHandoverFeature" + mInCallHandoverFeature);
                         if(mFeatureSwitchRequest != null && mFeatureSwitchRequest.mEventCode == ACTION_START_HANDOVER && serviceId == mFeatureSwitchRequest.mServiceId){
@@ -1921,6 +1918,11 @@ class MyVoWifiCallback implements VoWifiCallback {
                                 mFeatureSwitchRequest = null;
                                 Log.i(TAG,"onSessionEmpty-> This is volte call,so mFeatureSwitchRequest has been emptyed.");
                             }
+                        }
+                        //SPRD:modify by bug687400
+                        if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
+                            Log.i(TAG,"onSessionEmpty->mCurrentImsFeature:"+mCurrentImsFeature);
+                            updateInCallState(false);
                         }
                         mInCallHandoverFeature = ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN;
                         if (!mPendingAttachVowifiSuccess && !mPendingActivePdnSuccess ) {
