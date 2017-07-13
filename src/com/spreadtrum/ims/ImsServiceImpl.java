282992e197cc4ba43cec3bc12721880b6c31432b
xu.peng <xu.peng@spreadtrum.com>
2017-07-13 17:03:08 +0800

    Bug #701983 Not able register to Volte after dropped to LTE sometimes
    [root cause  ] Ims register status is wrong in special situation
    [changes     ] Correct ims register status in special situation
    [side effects] no
    [self test   ]<board, Test cases summary>:<9850, test volte register status>
    [reviewers   ] zhiwei.fu
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I0748ad1ed33ee9dfe4e557f293f1f3dbf7e5b14e

--- src/com/spreadtrum/ims/ImsServiceImpl.java
+++ src/com/spreadtrum/ims/ImsServiceImpl.java
@@ -209,10 +209,18 @@ public class ImsServiceImpl {
                     if (ar.exception == null && ar.result != null) {
                         int[] responseArray = (int[])ar.result;
                         mImsServiceState.mRegState = responseArray[0];
-                        // SPRD 681641
-                        if (responseArray != null && responseArray.length >1) {
-                            mImsServiceState.mImsRegistered = (responseArray[0] != 0 && responseArray[1]== 1);
-                            mImsServiceState.mSrvccState = -1;
+                        // SPRD 681641 701983
+                        if (responseArray != null) {
+                            if (responseArray.length >1) {
+                                mImsServiceState.mImsRegistered = (responseArray[0] != 0 && responseArray[1]== 1);
+                            } else if (responseArray.length == 1) {
+                                mImsServiceState.mImsRegistered = (mImsServiceState.mRegState == IMS_REG_STATE_REGISTERED
+                                    ? true : false);
+                            }
+                            Log.i(TAG, "EVENT_IMS_STATE_CHANGED --mImsServiceState.mImsRegistered : "
+                                        + mImsServiceState.mImsRegistered
+                                        + " | responseArray.length = "
+                                        + responseArray.length);
                         }
                         Log.i(TAG,"EVENT_IMS_STATE_CHANGED->mRegState:"+mImsServiceState.mRegState);
                         switch(mImsServiceState.mRegState){
