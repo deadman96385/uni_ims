c8e2077c38ca7dc857a699d99cd34d846e7a6a91
shuo.guan <shuo.guan@spreadtrum.com>
2017-07-20 17:16:10 +0800

    Bug #710956 volte porting PikeL4.4
    [root cause ] PikeL4.4 volte problem
    [changes ] add vowifi feature to branch PikeL4.4
    [side effects] no
    [self test ] <board, Test cases summary>:<board: 9820><test case: following bug test steps, test 5 times, pass>
    [reviewers ] lei.huang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ie227b23781ff4a804438b74c2df7f02affd1aa04

--- ims/src/com/spreadtrum/ims/ImsService.java
+++ ims/src/com/spreadtrum/ims/ImsService.java
@@ -2097,7 +2097,6 @@ class MyVoWifiCallback implements VoWifiCallback {
             isAirplaneModeOn = Settings.Global.getInt(getApplicationContext().getContentResolver(),
                     Settings.Global.AIRPLANE_MODE_ON, 0) > 0;
             if(state == ImsPDNStatus.IMS_PDN_ACTIVE_FAILED ||isAirplaneModeOn){
-                mPendingCPSelfManagement = false;
                 if(mPendingCPSelfManagement || mFeatureSwitchRequest == null){
                     ImsServiceImpl service = mImsServiceImplMap.get(
                             Integer.valueOf(mTelephonyManager.getPrimaryCard()+1));
@@ -2105,6 +2104,7 @@ class MyVoWifiCallback implements VoWifiCallback {
                        service.setIMSRegAddress(null);
                     }
                 }
+                mPendingCPSelfManagement = false;
             }
         }
         Log.i(TAG,"onImsPdnStatusChange->serviceId:"+serviceId +" state:" + state
@@ -2290,7 +2290,11 @@ class MyVoWifiCallback implements VoWifiCallback {
     }
 
     public void notifyCPVowifiAttachSucceed(){
-        mHandler.sendMessageDelayed(mHandler.obtainMessage(EVENT_NOTIFY_CP_VOWIFI_ATTACH_SUCCESSED),1000);
+       // mHandler.sendMessageDelayed(mHandler.obtainMessage(EVENT_NOTIFY_CP_VOWIFI_ATTACH_SUCCESSED),1000);
+        Log.i(TAG,"EVENT_NOTIFY_CP_VOWIFI_ATTACH_SUCCESSED-> notifyImsHandoverStatus:" + ImsHandoverResult.IMS_HANDOVER_ATTACH_SUCCESS);
+        ImsServiceImpl Impl = mImsServiceImplMap.get(
+                Integer.valueOf(mTelephonyManager.getPrimaryCard()+1));
+        Impl.notifyImsHandoverStatus(ImsHandoverResult.IMS_HANDOVER_ATTACH_SUCCESS);
     }
 
     public void notifyCpCallEnd(){
