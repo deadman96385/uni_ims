74f20bc361248e6ffc4cecb87b483d5ff4336c75
fantao.meng <fantao.meng@spreadtrum.com>
2017-08-17 09:16:06 +0800

    Bug #726983 change sequence after Vowifi call end
    [root cause  ] after Vowifi call, we need to set mFeatureSwitchRequest first
    [changes     ] change sequence between set mFeatureSwitchRequest and mIsVowifiCall
    [side effects]No
    [self test   ]: Vowifi and VoLTE call handover
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:No
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:8950
    [test case]:Vowifi and VoLTE call handover
    [reviewers   ] lei.huang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ic1f067605e486ae4da2bc4a9bf450d9b77a4ff91
    (cherry picked from commit 3d70e2530be92ba2e0ec74346f7507daa6e08bf4)

--- src/com/spreadtrum/ims/ImsService.java
+++ src/com/spreadtrum/ims/ImsService.java
@@ -412,10 +412,6 @@ public class ImsService extends Service {
                                             Integer.valueOf(mFeatureSwitchRequest.mServiceId));
                                     if (currentService != null) {
                                         if (currentService.isVolteSessionListEmpty() && currentService.isVowifiSessionListEmpty()) {
-                                            if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
-                                                Log.i(TAG,"EVENT_WIFI_ALL_CALLS_END->mCurrentImsFeature:"+mCurrentImsFeature);
-                                                updateInCallState(false);
-                                            }
                                             mCallEndType = CallEndEvent.WIFI_CALL_END;
                                             if (mInCallHandoverFeature != mFeatureSwitchRequest.mTargetType) {
                                                 if (mFeatureSwitchRequest.mTargetType == ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_WIFI) {
@@ -441,6 +437,10 @@ public class ImsService extends Service {
                                             if(mIsVowifiCall && mCurrentImsFeature == ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_WIFI
                                                     && mFeatureSwitchRequest != null && !mPendingAttachVowifiSuccess && !mPendingActivePdnSuccess) {
                                                 mFeatureSwitchRequest = null;
+                                            }
+                                            if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
+                                                Log.i(TAG,"EVENT_WIFI_ALL_CALLS_END->mCurrentImsFeature:"+mCurrentImsFeature);
+                                                updateInCallState(false);
                                             }
                                         }
                                     } else {
