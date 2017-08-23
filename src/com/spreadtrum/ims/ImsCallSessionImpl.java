17b7862b166cbc0d409ba3cd5fcf261d06af9b71
donghy <donghy@spreadst.com>
2017-08-23 11:48:33 +0800

    Bug #709679 Local Tone Feature. [ims]
    [root cause  ] new feature
    [changes     ] Local Tone Feature.
    [side effects] No
    [self test   ] <sp9850><follow test steps,test 10 times,pass><test case TE3,pass>
    [reviewers   ] lisj caosuyun
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I4fa71c28a6ca49b4290d54ad8a107e6ad5ddd38c
    (cherry picked from commit aee45b7e4abacf21e0eb39afa321a97ad9a438cb)

--- src/com/spreadtrum/ims/ImsCallSessionImpl.java
+++ src/com/spreadtrum/ims/ImsCallSessionImpl.java
@@ -100,6 +100,7 @@ public class ImsCallSessionImpl extends IImsCallSession.Stub {
     private static final String ACTION_SUPP_SERVICE_NOTIFICATION =
             "com.android.ACTION_SUPP_SERVICE_NOTIFICATION";
     private static final String SUPP_SERV_CODE_EXTRA = "supp_serv_code";
+    private static final String SUPP_SERV_NOTIFICATION_TYPE_EXTRA = "supp_serv_notification_type";
     /* @} */
 
     public ImsCallSessionImpl(ImsCallProfile profile, IImsCallSessionListener listener, Context context,
@@ -1595,9 +1596,11 @@ public class ImsCallSessionImpl extends IImsCallSession.Stub {
         /* SPRD: Local Tone Feature. @{ */
         if (notification != null) {
             int code = notification.code;
+            int notificationType = notification.notificationType;
             Intent intent = new Intent();
             intent.setAction(ACTION_SUPP_SERVICE_NOTIFICATION);
             intent.putExtra(SUPP_SERV_CODE_EXTRA, code);
+            intent.putExtra(SUPP_SERV_NOTIFICATION_TYPE_EXTRA, notificationType);
             mContext.sendBroadcast(intent);
         }
         /* @} */
