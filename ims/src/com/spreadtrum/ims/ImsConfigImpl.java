212c399bd08e48d76b5600c4b51337105d37f833
longchen.chen <longchen.chen@spreadtrum.com>
2017-10-31 16:17:21 +0800

    Bug #760078  [SC9820A][TECHAINSH][LF07B][Smartfren]VoLTE: video conf calls
    [root cause  ] Camera resolution is wrong
    [changes     ] Set it to QVGA30(3)
    [side effects] no
    [self test   ]:
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: 9820
    [test case]: can merge conf calls when insert Smartfren sim cards
    [reviewers   ] longchen.chen
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I39c29294b68a7e233848945430cacfc5bbec3516

--- ims/src/com/spreadtrum/ims/ImsConfigImpl.java
+++ ims/src/com/spreadtrum/ims/ImsConfigImpl.java
@@ -46,8 +46,8 @@ public class ImsConfigImpl extends IImsConfig.Stub {
     private Context mContext;
     private SharedPreferences mSharedPreferences;
     private static final String VIDEO_CALL_RESOLUTION = "vt_resolution";
-    private int mCameraResolution = VT_RESOLUTION_VGA_REVERSED_30;
-    public int mDefaultVtResolution = VT_RESOLUTION_VGA_REVERSED_30;
+    private int mCameraResolution = VT_RESOLUTION_QVGA_REVERSED_30;
+    public int mDefaultVtResolution = VT_RESOLUTION_QVGA_REVERSED_30;
 
     /**
      * Creates the Ims Config interface object for a sub.
@@ -156,7 +156,9 @@ public class ImsConfigImpl extends IImsConfig.Stub {
                     break;
                 case EVENT_VOLTE_CALL_DEDINE_MEDIA_TYPE:
                     String[] cmd=new String[1];
+                    mCameraResolution = VT_RESOLUTION_QVGA_REVERSED_30;
                     cmd[0] = "AT+CDEFMP=1,\""+mCameraResolution+"\"";
+                    Log.d(TAG,"EVENT_VOLTE_CALL_DEDINE_MEDIA_TYPE->cmd[0]:"+cmd[0]);
                     mCi.invokeOemRilRequestStrings(cmd, null);
                     break;
                 default:
