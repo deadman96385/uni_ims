8e3da88236baaf423ef6c8e35bee4d27b4ca846b
rongmei.li <rongmei.li@spreadtrum.com>
2018-03-13 09:25:30 +0800

    Bug #840308 start wifi call fail
    [root cause  ] mFeatureSwitchRequest is not null when WiFi call end lead to make call fail.
    [changes     ] set mFeatureSwitchRequest null when WiFi call end.
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sp9820w
    [test case]:Vowifi and VoLTE call handover
    [reviewers   ] lei.huang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I027f965df5bc06acdded396abe853a44c82a7a51

--- src/com/spreadtrum/ims/ImsService.java
+++ src/com/spreadtrum/ims/ImsService.java
@@ -409,10 +409,6 @@ public class ImsService extends Service {
                                             Integer.valueOf(mFeatureSwitchRequest.mServiceId));
                                     if (currentService != null) {
                                         if (currentService.isVolteSessionListEmpty() && currentService.isVowifiSessionListEmpty()) {
-                                            if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
-                                                Log.i(TAG,"EVENT_WIFI_ALL_CALLS_END->mCurrentImsFeature:"+mCurrentImsFeature);
-                                                updateInCallState(false);
-                                            }
                                             mCallEndType = CallEndEvent.WIFI_CALL_END;
                                             if (mInCallHandoverFeature != mFeatureSwitchRequest.mTargetType) {
                                                 if (mFeatureSwitchRequest.mTargetType == ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_WIFI) {
@@ -438,6 +434,11 @@ public class ImsService extends Service {
                                             if(mIsVowifiCall && mCurrentImsFeature == ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_WIFI
                                                     && mFeatureSwitchRequest != null && !mPendingAttachVowifiSuccess && !mPendingActivePdnSuccess) {
                                                 mFeatureSwitchRequest = null;
+                                            }
+                                            // SPRD:fix for bug840308
+                                            if(mCurrentImsFeature != ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN){
+                                                Log.i(TAG,"EVENT_WIFI_ALL_CALLS_END->mCurrentImsFeature:"+mCurrentImsFeature);
+                                                updateInCallState(false);
                                             }
                                         }
                                     } else {
@@ -2417,8 +2418,9 @@ class MyVoWifiCallback implements VoWifiCallback {
                 && imsService.getSrvccState() == VoLteServiceState.HANDOVER_COMPLETED){
             imsService.setSrvccState(-1);
         }
-        // SPRD add for bug 826630
-        if (!isInCall) {
+        // SPRD add for bug 826630 and bug840308
+        iLog("updateInCallState->mWifiRegistered:"+mWifiRegistered+" mVolteRegistered:"+mVolteRegistered +" mCurrentImsFeature:"+mCurrentImsFeature);
+        if (!isInCall && !mWifiRegistered && !mVolteRegistered && (mCurrentImsFeature == ImsConfig.FeatureConstants.FEATURE_TYPE_UNKNOWN)) {
             if (mIsVowifiCall) {
                 mIsVowifiCall = false;
             } else if (mIsVolteCall) {
@@ -2431,7 +2433,7 @@ class MyVoWifiCallback implements VoWifiCallback {
         }
 
         iLog("updateInCallState->isInCall:"+isInCall+" mIsWifiCalling:"+mIsWifiCalling
-                +" inCallPhoneId:"+mInCallPhoneId + " mIsVolteCall:" + mIsVolteCall);
+                +" inCallPhoneId:"+mInCallPhoneId + " mIsVolteCall:" + mIsVolteCall+" mIsVowifiCall:"+mIsVowifiCall);
     }
 
     public void notifySrvccCapbility(int cap){
